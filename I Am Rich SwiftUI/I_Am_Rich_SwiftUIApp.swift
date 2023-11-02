d4e52896c46fd169f6956ee02257530185605f86
pankaj104 <pankajkumar1122111@gmail.com>
2023-11-02 17:01:17 +0530

    first commit

---
+++ I Am Rich SwiftUI/I_Am_Rich_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_SwiftUIApp.swift
+//  I Am Rich SwiftUI
+//
+//  Created by Pankaj Kumar on 12/09/22.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
